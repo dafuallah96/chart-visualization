29ce1600c834771a02c955e9607389b15d5a7732
Ahmad Siraj MY <28752140+asbmy@users.noreply.github.com>
2020-04-04 22:27:57 +0800

    attemp 1

--- src/app/params/params.component.ts
+++ src/app/params/params.component.ts
@@ -9,7 +9,40 @@ export class ParamsComponent implements OnInit {
 
   constructor() { }
 
+  public chartType: string = "pie";
+  public chartLabel: string[] = [];
+  public chartData: number[] = [];
+
+  public chartLastLabel: string[] = [];
+  public chartLastData: number[] = [];
+
+  chartFilled() {
+    this.chartLabel.push('Null', 'Forced Closed', 'Closed', 'GM_Terminated');
+    this.chartData.push(16.67, 18.25, 61.9, 3.17);
+
+    this.chartLastLabel.push('1', '2');
+    this.chartLastData.push(9.35, 90.65);
+  }
+
+  chartOptions = {
+
+    animation: {
+      duration: 1000,
+      easing: "easeInOutQuad"
+    },
+
+    responsive: true,
+    legend: {
+      display: true,
+      position: "right",
+      fullWidth: false,
+      reverse: true
+    },
+  };
+
+
   ngOnInit() {
+    this.chartFilled();
   }
 
 }
